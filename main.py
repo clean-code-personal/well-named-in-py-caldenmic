6a844389759f975651388d3a9cca9a2e51606c3a
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-06 10:54:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cable_functions import color_pair_to_string, get_color_from_pair_number, get_pair_number_from_color, test_number_to_pair, test_pair_to_number
+from cable_reference_manual import print_reference_manual
 
 if __name__ == '__main__':
+  print_reference_manual()
   test_number_to_pair(4, 'White', 'Brown')
   test_number_to_pair(5, 'White', 'Slate')
   test_pair_to_number('Black', 'Orange', 12)
