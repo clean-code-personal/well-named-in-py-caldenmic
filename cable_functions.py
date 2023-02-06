2571c532b9a85f2d629704276a28d0edb4463a73
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-06 10:58:04 +0530

    Update cable_functions.py

--- cable_functions.py
+++ cable_functions.py
@@ -1,6 +1,5 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
-REFERENCE_MANUAL = dict()
 
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'
