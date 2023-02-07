6a32f9ea028296e7dc6b33bf4244ca4b165c23ff
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-07 11:05:34 +0530

    Update cable_test_functions.py

--- test/cable_test_functions.py
+++ test/cable_test_functions.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.append('..')
+sys.path.append('..')
 from cable_functions import get_color_from_pair_number, get_pair_number_from_color 
 
 def test_number_to_pair(pair_number, expected_major_color, expected_minor_color):
